4f2ba07ce3b071a1b13974a135159456cc452792
Andrew Reeves <andrewpaulreeves@gmail.com>
2016-03-09 15:32:33 +0000

    add version tracking

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import versioneer
+
+setup(
+    name='aotools',
+    author_email='a.p.reeves@durham.ac.uk',
+    packages=['aotools', 'aotools.circle'],
+    description='A set of useful functions for Adaptive Optics in Python',
+    long_description=open('README.md').read(),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass()
+)
