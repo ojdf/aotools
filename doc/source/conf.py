6c07999f654393d5c84a1b1065f7ea3f51ddde28
Andrew Reeves <andrewpaulreeves@gmail.com>
2016-03-10 10:23:05 +0000

    update doc conf

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,6 +15,10 @@
 import sys
 import os
 
+# Add soapy to path
+AOTOOLS_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../..')
+sys.path.append(AOTOOLS_DIR)
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -36,7 +40,8 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
+    'sphinx.ext.viewcode'
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -54,9 +59,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'aotools'
-copyright = u'2016, Andrew Reeves'
-author = u'Andrew Reeves'
+project = u'AOtools'
+copyright = u'2016, Centre for Advanced Instrumentation'
+author = u'Centre for Advanced Instrumentation'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -116,7 +121,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
