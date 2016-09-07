24789ae7cfbddf13ec9eaefb1b728a14069e65df
Andrew Reeves <a.p.reeves@durham.ac.uk>
2016-09-07 13:46:50 +0100

    change test imports

--- test/testphasescreen.py
+++ test/testphasescreen.py
@@ -1,8 +1,8 @@
-from aotools import phasescreen
+from aotools import turbulence
 
 def test_ftScrn():
 
-    scrn = phasescreen.ft_phase_screen(0.2, 512, 4.2/128, 30., 0.01)
+    scrn = turbulence.ft_phase_screen(0.2, 512, 4.2/128, 30., 0.01)
 
 def test_ftShScrn():
-    scrn = phasescreen.ft_sh_phase_screen(0.2, 512, 4.2/128, 30., 0.01)
+    scrn = turbulence.ft_sh_phase_screen(0.2, 512, 4.2/128, 30., 0.01)
