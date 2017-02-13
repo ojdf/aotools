3fbc258bfa34acbb73e41c1a1f8f7cce66aceab5
Andrew Reeves <a.p.reeves@durham.ac.uk>
2017-02-13 15:04:42 +0000

    updated doc strings for Fried infinite phase screen

--- aotools/turbulence/infinitephasescreen_fried.py
+++ aotools/turbulence/infinitephasescreen_fried.py
@@ -22,7 +22,7 @@ def find_allowed_size(nx_size):
 
 class PhaseScreen(object):
     """
-    A "Phase Screen" for use in AO simulation. 
+    A "Phase Screen" for use in AO simulation using the Fried method.
     
     This represents the phase addition light experiences when passing through atmospheric 
     turbulence. Unlike other phase screen generation techniques that translate a large static 
@@ -30,8 +30,9 @@ class PhaseScreen(object):
     steps as required. This can significantly reduce memory consuption at the expense of more 
     processing power required.
     
-    The technique is described in a paper by Assemat and Wilson, 2006. It essentially assumes that
-    there are two matrices, "A" and "B", that can be used to extend an existing phase screen.
+    The technique is described in a paper by Assemat and Wilson, 2006 and expanded upon by Fried, 2008.
+    It essentially assumes that there are two matrices, "A" and "B",
+    that can be used to extend an existing phase screen.
     A single row or column of new phase can be represented by 
     
         X = A.Z + B.b
@@ -57,18 +58,20 @@ class PhaseScreen(object):
         U, w, U^t = svd(BB^t)
     
     L is a diagonal matrix where the diagonal elements are w^(1/2).    
-     
+
+    The Z data is taken from points in a "stencil" defined by Fried that samples the entire screen.
+    An additional "reference point" is also considered, that
+
     On initialisation an initial phase screen is calculated using an FFT based method.
-    When 'addRows' is called, a new vector of phase is added to the phase screen using `nCols`
-    columns of previous phase. Assemat & Wilson claim that two columns are adequate for good
-    atmospheric statistics. The phase in the screen data is always accessed as `<phasescreen>.scrn`.
+    When 'addRows' is called, a new vector of phase is added to the phase screen.
     
     Parameters:
         nx_size (int): Size of phase screen (NxN)
-        pxlScale(float): Size of each phase pixel in metres
+        pixel_scale(float): Size of each phase pixel in metres
         r0 (float): fried parameter (metres)
         L0 (float): Outer scale (metres)
-        nCol (int, optional): Number of columns to use to continue screen, default is 2
+        random_seed (int, optional): seed for the random number generator
+        stencil_length_factor (int, optional): How much longer is the stencil than the desired phase? default is 4
     """
     
     def __init__(self, nx_size, pixel_scale, r0, L0, random_seed=None, stencil_length_factor=4):
@@ -104,8 +107,6 @@ class PhaseScreen(object):
         """
         Sets the coords of X, the new phase vector.
 
-        This is always just to teh right of the existing phase. Hence the coordinate system is defined here, where
-        the x co-ordinate is -1 for all points on the new phase.
         """
 
         self.X_coords = numpy.zeros((self.nx_size, 2))
@@ -145,7 +146,9 @@ class PhaseScreen(object):
         self.n_stencils = len(self.stencil_coords)
 
     def calc_seperations(self):
-
+        """
+        Calculates the seperations between the phase points in the stencil and the new phase vector
+        """
         positions = numpy.append(self.stencil_positions, self.X_positions, axis=0)
         self.seperations = numpy.zeros((len(positions), len(positions)))
 
@@ -160,6 +163,9 @@ class PhaseScreen(object):
                 self.seperations[i, j] = delta_r
 
     def make_covmats(self):
+        """
+        Makes the covariance matrices required for adding new phase
+        """
         self.cov_mat = phaseCovariance(self.seperations, self.r0, self.L0)
 
         self.cov_mat_zz = self.cov_mat[:self.n_stencils, :self.n_stencils]
@@ -170,49 +176,30 @@ class PhaseScreen(object):
     def makeAMatrix(self):
         """
         Calculates the "A" matrix, that uses the existing data to find a new 
-        component of the new phase vector. This is for propagating in axis 0.
+        component of the new phase vector.
         """
         # Different inversion methods, not sure which is best
         cf = linalg.cho_factor(self.cov_mat_zz)
         inv_cov_zz = linalg.cho_solve(cf, numpy.identity(self.cov_mat_zz.shape[0]))
-        # inv_cov_zz = numpy.linalg.pinv(self.cov_zz)
-        
+
         self.A_mat = self.cov_mat_xz.dot(inv_cov_zz)
 
     def makeBMatrix(self):
         """
-        Calculates the "B" matrix, that turns a random vector into a component of the new phase. 
-        
-        Finds a B matrix for the case of generating new data on each side of the phase screen.
-        """
-        # Axis 0, forwards
-        self.B_mat = self.makeSingleBMatrix(
-                self.cov_mat_xx, self.cov_mat_zx, self.A_mat)
-
-
-    def makeSingleBMatrix(self, cov_xx, cov_zx, A_mat):
-        """
-        Makes the B matrix for a single direction
-        
-        Parameters:
-            cov_xx: Matrix of XX covariance
-            cov_zx: Matrix of ZX covariance
-            A_mat: Corresponding A matrix
+        Calculates the "B" matrix, that turns a random vector into a component of the new phase.
         """
         # Can make initial BBt matrix first
-        BBt = cov_xx - A_mat.dot(cov_zx)
-        
+        BBt = self.cov_mat_xx - self.A_mat.dot(self.cov_mat_zx)
+
         # Then do SVD to get B matrix
         u, W, ut = numpy.linalg.svd(BBt)
-        
+
         L_mat = numpy.zeros((self.nx_size, self.nx_size))
         numpy.fill_diagonal(L_mat, numpy.sqrt(W))
-        
+
         # Now use sqrt(eigenvalues) to get B matrix
-        B_mat = u.dot(L_mat) 
-        
-        return B_mat
-    
+        self.B_mat = u.dot(L_mat)
+
     def makeInitialScrn(self):
         """
         Makes the initial screen usign FFT method that can be extended 
@@ -244,9 +231,11 @@ class PhaseScreen(object):
 
         self._scrn = numpy.append(new_row, self._scrn, axis=0)[:self.stencil_length, :self.nx_size]
 
+        return self.scrn
+
     @property
     def scrn(self):
-        return self._scrn[:self.nx_size]
+        return self._scrn[:self.requested_nx_size, :self.requested_nx_size]
 
 
 
