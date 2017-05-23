5be20fc480ea1c17c150967f137afdc681d1c7e2
Matthew Townson <matthew.townson@durham.ac.uk>
2017-05-23 20:46:32 +0100

    Testing interpolation routines

---
+++ test/test_interpolation.py
@@ -0,0 +1,32 @@
+import numpy
+from aotools import interpolation
+
+
+def test_zoom():
+    image = numpy.random.random((10, 10))
+    zoomed_image = interpolation.zoom(image, (50, 50))
+    assert zoomed_image.shape == (50, 50)
+
+
+def test_zoom_with_order():
+    image = numpy.random.random((10, 10))
+    zoomed_image = interpolation.zoom(image, (50, 50), order=2)
+    assert zoomed_image.shape == (50, 50)
+
+
+def test_bin():
+    image = numpy.random.random((50, 50))
+    binned_image = interpolation.binImgs(image, 5)
+    assert binned_image.shape == (10, 10)
+
+
+def test_zoom_rbs():
+    image = numpy.random.random((10, 10))
+    zoomed_image = interpolation.zoom_rbs(image, (50, 50))
+    assert zoomed_image.shape == (50, 50)
+
+
+def test_zoom_rbs_with_order():
+    image = numpy.random.random((10, 10))
+    zoomed_image = interpolation.zoom_rbs(image, (50, 50), order=2)
+    assert zoomed_image.shape == (50, 50)
