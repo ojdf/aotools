51cb3c83b0fb83dfdf1be3ef14b73edefb203e45
Matthew Townson <matthew.townson@durham.ac.uk>
2018-01-16 11:57:41 +0000

    Remove redundant test code from module

--- aotools/turbulence/temporal_ps.py
+++ aotools/turbulence/temporal_ps.py
@@ -116,13 +116,3 @@ def fit_tps(tps, t_axis, D, V_guess=10, f_noise_guess=20, A_guess=9, tps_err=Non
             method="COBYLA")
 
     print(opt_result)
-
-
-if __name__ == "__main__":
-
-    from astropy.io import fits
-
-    data = fits.getdata("t0_5ms_canary_slopes.fits")
-
-    tps, tps_err, t_data = plot_tps(data, 150)
-    fit_tps(tps[0], t_data, D=4.2/7, V_guess=20, f_noise_guess=50, A_guess=9, tps_err=tps_err[0], plot=True)
