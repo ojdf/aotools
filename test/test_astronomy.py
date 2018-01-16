76b149b293c93c7debecb90217eee976ea15ed50
Matthew Townson <matthew.townson@durham.ac.uk>
2018-01-16 11:36:49 +0000

    Upate tests for new functions
    Added tests for the moved functions

--- test/test_astronomy.py
+++ test/test_astronomy.py
@@ -21,3 +21,8 @@ def test_magnitude_to_flux_and_flux_to_magnitude():
     flux = astronomy.magnitude_to_flux(5.56, 'V')
     magnitude = astronomy.flux_to_magnitude(flux, 'V')
     assert magnitude == 5.56
+
+
+def test_photons_per_band():
+    photons = astronomy.photons_per_band(5.56, circle(2, 5), 0.5, 0.001)
+    assert type(photons) == float
--- test/test_image_processing.py
+++ test/test_image_processing.py
@@ -28,3 +28,19 @@ def test_rms_contrast():
     image = numpy.random.random((20, 20))
     contrast = image_processing.rms_contrast(image)
     assert type(contrast) == float
+
+
+def test_encircled_energy():
+    data = numpy.random.rand(32, 32)
+    ee50d = image_processing.encircled_energy(data)
+    print(ee50d)
+    assert type(ee50d) == float
+
+
+def test_encircled_energy_func():
+    data = numpy.random.rand(32, 32)
+    x, y = image_processing.encircled_energy(data, eeDiameter=False)
+    print(y.min(), y.max())
+    assert len(x) == len(y)
+    assert numpy.max(y) <= 1
+    assert numpy.min(y) >= 0
