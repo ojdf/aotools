d145594c7bb66b8507165a9a650560997c9612d0
Matthew Townson <matthew.townson@durham.ac.uk>
2020-02-28 10:23:31 +0000

    Update setup information
    Remove Python 2 and update author email addresses

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst", "r") as readme:
 
 setup(
     name='aotools',
-    author_email='a.p.reeves@durham.ac.uk',
+    author_email='andrewpaulreeves@gmail.com, matthew.townson@durham.ac.uk',
     url='https://github.com/aotools/aotools',
     packages=['aotools',
               'aotools.astronomy',
@@ -26,10 +26,7 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
